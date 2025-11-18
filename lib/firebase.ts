12c3fca5ccb0597f34337ce2a590773e85eee793
TasvirLimbani <tasvirlimbani111@gmail.com>
2025-11-18 19:31:33 +0530

    firebase added
    firebase added

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,12 +4,18 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBpwJRbuHaMUpzd05NYDJM9uuwyH_Uvlr0",
+  authDomain: "e-commerce-v0.firebaseapp.com",
+  projectId: "e-commerce-v0",
+  storageBucket: "e-commerce-v0.firebasestorage.app",
+  messagingSenderId: "1064166904610",
+  appId: "1:1064166904610:web:1d8ff24daa03c891ed4b7b",
+  // apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  // authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  // projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  // appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
